d4b878f231109cb89122e8074d1adc8ee6958529
Mark Scheel <scheel@tapir.caltech.edu>
2018-01-25 17:49:05 -0800

    Add code for HorizonManager and test of it.

---
+++ src/ApparentHorizons/HorizonManager.hpp
@@ -0,0 +1,204 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+#include <tuple>
+#include <utility>
+
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "Parallel/Algorithm.hpp"
+#include "Parallel/ConstGlobalCache.hpp"
+#include "Parallel/Info.hpp"
+#include "Parallel/Printf.hpp"
+#include "Utilities/Literals.hpp"
+#include "Utilities/Requires.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+namespace Actions {
+
+struct PrintCountOfElementsAssociatedWithHorizonManager {
+  using apply_args = tmpl::list<>;
+
+  template <
+      typename DbTags, typename... InboxTags, typename Metavariables,
+      typename ArrayIndex, typename ActionList, typename ParallelComponent,
+      Requires<tmpl::list_contains_v<
+          DbTags, typename Metavariables::number_of_elements_tag>> = nullptr>
+  static auto apply(db::DataBox<DbTags>& box,  // HorizonManager's box
+                    tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    using number_of_elements_tag =
+        typename Metavariables::number_of_elements_tag;
+    const auto& num_elements = db::get<number_of_elements_tag>(box);
+    Parallel::printf("Number of elements on proc %d is %d\n",
+                     Parallel::my_proc(), num_elements);
+    return std::forward_as_tuple(std::move(box));
+  }
+};
+
+struct ReceiveCountOfElementsAssociatedWithHorizonManager {
+  using apply_args = tmpl::list<>;
+
+  template <
+      typename DbTags, typename... InboxTags, typename Metavariables,
+      typename ArrayIndex, typename ActionList, typename ParallelComponent,
+      Requires<tmpl::list_contains_v<
+          DbTags, typename Metavariables::number_of_elements_tag>> = nullptr>
+  static auto apply(db::DataBox<DbTags>& box,  // HorizonManager's box
+                    tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    using number_of_elements_tag =
+        typename Metavariables::number_of_elements_tag;
+    db::mutate<number_of_elements_tag>(
+        box, [](auto& num_elements) noexcept { ++num_elements; });
+    return std::forward_as_tuple(std::move(box));
+  }
+};
+
+template <typename ParallelComponentOfReceiver>
+struct CallReceiveCountOfElementsAssociatedWithHorizonManager {
+  using apply_args = tmpl::list<>;
+
+  template <typename DbTags, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent>
+  static auto apply(db::DataBox<DbTags>& box,
+                    tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::ConstGlobalCache<Metavariables>& cache,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    auto& receiver_proxy =
+        Parallel::get_parallel_component<ParallelComponentOfReceiver>(cache);
+
+    receiver_proxy.ckLocalBranch()
+        ->template explicit_single_action<
+            ReceiveCountOfElementsAssociatedWithHorizonManager>();
+
+    return std::forward_as_tuple(std::move(box));
+  }
+};
+
+struct InitCountOfElementsAssociatedWithHorizonManager {
+  using apply_args = tmpl::list<>;
+
+  template <typename DbTags, typename... InboxTags, typename Metavariables,
+            typename ArrayIndex, typename ActionList,
+            typename ParallelComponent>
+  static auto apply(db::DataBox<DbTags>& /*box*/,  // HorizonManager's box
+                    tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    using number_of_elements_tag =
+        typename Metavariables::number_of_elements_tag;
+    return std::make_tuple(db::create<typelist<number_of_elements_tag>>(0_st));
+  }
+};
+
+//-:  template<typename GhVars>
+//-:  struct VolumeDataForHorizonManager {
+//-:    Element<3> element;
+//-:    Index<3> extents;
+//-:    GhVars gh_vars;
+//-:  };
+//-:
+//-:struct VolumeDataFromElement {
+//-:
+//-:  template <typename DbTags, typename... InboxTags, typename Metavariables,
+//-:            typename ArrayIndex>
+//-:  static bool is_ready(
+//-:      const db::DataBox<DbTags>& box,
+//-:      const tuples::TaggedTuple<InboxTags...>& inboxes,
+//-:      const Parallel::ConstGlobalCache<Metavariables>& /*cache*/,
+//-:      const ArrayIndex& /*array_index*/) noexcept {
+//-:
+//-:    using horizon_manager_tag = typename Metavariables::horizon_manager_tag;
+//-:    using number_of_elements_tag = typename
+// Metavariables::number_of_elements_tag;
+//-:    auto& inbox = tuples::get<horizon_manager_tag>(inboxes);
+//-:    const auto& time_id = db::get<Tags::TimeId>(box);
+//-:
+//-:    auto receives = inbox.find(time_id);
+//-:    const size_t num_receives =
+//-:        receives == inbox.end() ? 0 : receives->second.size();
+//-:
+//-:    const auto& number_of_elements = db::get<number_of_elements_tag>(box);
+//-:
+//-:    return num_receives == number_of_elements;
+//-:  }
+//-:
+//-:
+//-:  // Called by the Element to send volume data to a HorizonManager.
+//-:  template <typename DbTags, typename... InboxTags, typename Metavariables,
+//-:            typename ArrayIndex, typename ActionList,
+//-:            typename ParallelComponent>
+//-:  static auto apply(db::DataBox<DbTags>& box, // HorizonManager's box
+//-:                    tuples::TaggedTuple<InboxTags...>& inboxes, // Data from
+// element.
+//-:                    const Parallel::ConstGlobalCache<Metavariables>& cache,
+//-:                    const ArrayIndex& /*array_index*/,
+//-:                    const ActionList /*meta*/,
+//-:                    const ParallelComponent* const /*meta*/) noexcept {
+//-:    using gh_vars_tag  = typename Metavariables::gh_vars_tag;
+//-:    using horizon_manager_tag = typename Metavariables::horizon_manager_tag;
+//-:    auto& inbox = tuples::get<horizon_manager_tag>(inboxes);
+//-:    const auto& time_id = db::get<Tags::TimeId>(box);
+//-:    auto remote_data = std::move(inbox[time_id]);
+//-:    inbox.erase(time_id);
+//-:
+//-:    // we think remote_data is
+// std::unordered_map<element_id(),VolumeDataForHorizonManager>
+//-:
+//-:    // Here we assume that all the elements are available.
+//-:
+//-:    // Do everything HorizonManager does, up to calling
+//-:    // horizon_proxy.receive_interpolated_volume_data
+//-:
+//-:  }
+//-:};
+//-:
+//-:  template <typename ParallelComponentOfReceiver>
+//-:  struct SendVolumeDataToHorizonManager {
+//-:    template <typename DbTags, typename... InboxTags, typename
+// Metavariables,
+//-:              typename ArrayIndex, typename ActionList,
+//-:              typename ParallelComponent>
+//-:    static auto apply(db::DataBox<DbTags>& box, // Element's box
+//-:                      tuples::TaggedTuple<InboxTags...>& /* inboxes */,
+//-:                      const Parallel::ConstGlobalCache<Metavariables>&
+// cache,
+//-:                      const ArrayIndex& /*array_index*/,
+//-:                      const ActionList /*meta*/,
+//-:                      const ParallelComponent* const /*meta*/) noexcept {
+//-:      using gh_vars_tag  = typename Metavariables::gh_vars_tag;
+//-:      using horizon_manager_tag = typename
+// Metavariables::horizon_manager_tag;
+//-:      const auto& time_id = db::get<Tags::TimeId>(box);
+//-:      const auto& element = db::get<Tags::Element<3>>(box);
+//-:      const auto& extents = db::get<Tags::Extents<3>>(box);
+//-:      const auto& gh_vars = db::get<gh_vars_tag>(box);
+//-:
+//-:      auto& receiver_proxy =
+//-: Parallel::get_parallel_component<ParallelComponentOfReceiver>(cache);
+//-:
+//-:      receiver_proxy.ckLocalBranch().template
+//-:        receive_data<horizon_manager_tag>(time_id,
+//-:
+// std::make_pair(element.id(),
+// VolumeDataForHorizonManager<std::remove_cv_t<decltype(gh_vars)>>{
+// element,extents,std::move(gh_vars)}));
+//-:
+//-:      return std::forward_as_tuple(std::move(box));
+//-:  };
+
+}  // namespace Actions
---
+++ tests/Unit/ApparentHorizons/Test_HorizonManager.cpp
@@ -0,0 +1,153 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "AlgorithmArray.hpp"
+#include "AlgorithmGroup.hpp"
+#include "ApparentHorizons/HorizonManager.hpp"
+#include "DataStructures/DataBox/DataBox.hpp"
+#include "DataStructures/DataVector.hpp"
+#include "Domain/CoordinateMaps/Affine.hpp"
+#include "Domain/Domain.hpp"
+#include "Domain/DomainCreators/DomainCreator.hpp"
+#include "Domain/Element.hpp"
+#include "Domain/ElementIndex.hpp"
+#include "Domain/InitialElementIds.hpp"
+#include "Domain/Tags.hpp"
+#include "Parallel/Algorithm.hpp"
+#include "Parallel/ConstGlobalCache.hpp"
+#include "Parallel/InitializationFunctions.hpp"
+#include "Parallel/Main.hpp"
+#include "Utilities/TMPL.hpp"
+#include "Utilities/TaggedTuple.hpp"
+
+#include "Domain/DomainCreators/RegisterDerivedWithCharm.cpp"
+
+struct TestMetavariables;
+
+template <class Metavariables>
+struct HorizonManagerComponent {
+  using chare_type = Parallel::Algorithms::Group;
+  using const_global_cache_tag_list = tmpl::list<>;
+  using metavariables = Metavariables;
+  using action_list = tmpl::list<>;
+  using explicit_single_actions_list =
+      tmpl::list<Actions::PrintCountOfElementsAssociatedWithHorizonManager,
+                 Actions::ReceiveCountOfElementsAssociatedWithHorizonManager,
+                 Actions::InitCountOfElementsAssociatedWithHorizonManager>;
+  using initial_databox = db::DataBox<db::get_databox_list<
+      typelist<typename Metavariables::number_of_elements_tag>>>;
+  using options = typelist<>;
+  static void initialize(
+      Parallel::CProxy_ConstGlobalCache<Metavariables>& global_cache);
+  static void execute_next_global_actions(
+      const typename Metavariables::Phase next_phase,
+      const Parallel::CProxy_ConstGlobalCache<Metavariables>& global_cache) {
+    if (next_phase == Metavariables::Phase::CheckAnswer) {
+      auto& my_proxy = Parallel::get_parallel_component<
+          HorizonManagerComponent<Metavariables>>(
+          *(global_cache.ckLocalBranch()));
+      my_proxy.template explicit_single_action<
+          Actions::PrintCountOfElementsAssociatedWithHorizonManager>();
+    }
+  }
+};
+
+template <class Metavariables>
+void HorizonManagerComponent<Metavariables>::initialize(
+    Parallel::CProxy_ConstGlobalCache<Metavariables>& global_cache) {
+  auto& my_proxy = Parallel::get_parallel_component<HorizonManagerComponent>(
+      *(global_cache.ckLocalBranch()));
+  my_proxy.template explicit_single_action<
+      Actions::InitCountOfElementsAssociatedWithHorizonManager>();
+}
+
+template <class Metavariables>
+struct DgElementArray {
+  using chare_type = Parallel::Algorithms::Array;
+  using const_global_cache_tag_list = tmpl::list<>;
+  using metavariables = Metavariables;
+  using action_list = typelist<>;
+  using array_index = ElementIndex<3>;
+  using explicit_single_actions_list = tmpl::list<
+      Actions::CallReceiveCountOfElementsAssociatedWithHorizonManager<
+          HorizonManagerComponent<Metavariables>>>;
+  using initial_databox = db::DataBox<db::get_databox_list<typelist<>>>;
+  using options = typelist<typename Metavariables::domain_creator_tag>;
+  static void initialize(
+      Parallel::CProxy_ConstGlobalCache<Metavariables>& global_cache,
+      std::unique_ptr<DomainCreator<3, Frame::Inertial>>
+          domain_creator) noexcept;
+  static void execute_next_global_actions(
+      const typename Metavariables::Phase next_phase,
+      const Parallel::CProxy_ConstGlobalCache<Metavariables>& global_cache) {
+    if (next_phase == Metavariables::Phase::Setup) {
+      auto& dg_element_array = Parallel::get_parallel_component<DgElementArray>(
+          *(global_cache.ckLocalBranch()));
+
+      dg_element_array.template explicit_single_action<
+          Actions::CallReceiveCountOfElementsAssociatedWithHorizonManager<
+              HorizonManagerComponent<Metavariables>>>();
+    }
+  }
+};
+
+template <class Metavariables>
+void DgElementArray<Metavariables>::initialize(
+    Parallel::CProxy_ConstGlobalCache<Metavariables>& global_cache,
+    std::unique_ptr<DomainCreator<3, Frame::Inertial>>
+        domain_creator) noexcept {
+  auto& dg_element_array = Parallel::get_parallel_component<DgElementArray>(
+      *(global_cache.ckLocalBranch()));
+
+  auto domain = domain_creator->create_domain();
+  for (const auto& block : domain.blocks()) {
+    const auto initial_ref_levs =
+        domain_creator->initial_refinement_levels()[block.id()];
+    const std::vector<ElementId<3>> element_ids =
+        initial_element_ids(block.id(), initial_ref_levs);
+    for (size_t i = 0, which_proc = 0,
+                number_of_procs =
+                    static_cast<size_t>(Parallel::number_of_procs());
+         i < element_ids.size(); ++i) {
+      dg_element_array(ElementIndex<3>(element_ids[i]))
+          .insert(global_cache, which_proc);
+      which_proc = which_proc + 1 == number_of_procs ? 0 : which_proc + 1;
+    }
+  }
+  dg_element_array.doneInserting();
+}
+
+struct TestMetavariables {
+  using component_list = tmpl::list<HorizonManagerComponent<TestMetavariables>,
+                                    DgElementArray<TestMetavariables>>;
+  static constexpr const char* const help{"Test HorizonManager in parallel"};
+  static constexpr bool ignore_unrecognized_command_line_options = false;
+
+  struct number_of_elements_tag : db::DataBoxTag {
+    static constexpr db::DataBoxString label = "number_of_elements";
+    using type = size_t;
+  };
+
+  using domain_creator_tag = OptionTags::DomainCreator<3, Frame::Inertial>;
+
+  enum class Phase { Initialization, Setup, CheckAnswer, Exit };
+  static Phase determine_next_phase(const Phase& current_phase,
+                                    const Parallel::CProxy_ConstGlobalCache<
+                                        TestMetavariables>& /*cache_proxy*/) {
+    if (current_phase == Phase::Initialization) {
+      return Phase::Setup;
+    }
+    if (current_phase == Phase::Setup) {
+      return Phase::CheckAnswer;
+    }
+    return Phase::Exit;
+  }
+};
+
+static const std::vector<void (*)()> charm_init_node_funcs{
+    &setup_error_handling};
+static const std::vector<void (*)()> charm_init_proc_funcs{};
+
+using charm_metavariables = TestMetavariables;
+
+#include "Parallel/CharmMain.cpp"

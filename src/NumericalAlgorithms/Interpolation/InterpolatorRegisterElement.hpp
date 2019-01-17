c1959e94ea7380a124593edbbcfeb68a9b3299cb
Mark Scheel <scheel@tapir.caltech.edu>
2019-01-17 14:25:24 -0800

    Add RegisterElementWithInterpolator.
    This new Action appears to be necessary so that it can be called
    from the execute_next_phase function of DgElementArray.
    If RegisterElement is called directly from execute_next_phase, it
    is called once rather than once per DgElementArray element; the latter
    is desired.

--- src/NumericalAlgorithms/Interpolation/InterpolatorRegisterElement.hpp
+++ src/NumericalAlgorithms/Interpolation/InterpolatorRegisterElement.hpp
@@ -7,14 +7,25 @@
 #include "DataStructures/DataBox/DataBoxTag.hpp"
 #include "NumericalAlgorithms/Interpolation/Tags.hpp"
 #include "Parallel/ConstGlobalCache.hpp"
+#include "Parallel/Invoke.hpp"
 #include "Utilities/Gsl.hpp"
 #include "Utilities/TaggedTuple.hpp"
+
+/// \cond
+namespace intrp {
+template <typename Metavariables>
+struct Interpolator;
+}  // namespace intrp
+/// \endcond
 
 namespace intrp {
 namespace Actions {
 
 /// \ingroup ActionsGroup
-/// \brief Called by each local Element to register itself with an Interpolator.
+/// \brief Invoked on the Interpolator ParallelComponent to register an
+/// Element with the Interpolator.
+///
+/// This is called by RegisterElementWithInterpolator below.
 ///
 /// Uses: nothing
 ///
@@ -43,6 +54,40 @@ struct RegisterElement {
                                     num_elements) noexcept {
           ++(*num_elements);
         });
+  }
+};
+
+/// \ingroup ActionsGroup
+/// \brief Called from DgElementArray::execute_next_phase. This is invoked
+/// on the DgElementArray ParallelComponent to register all its Elements with
+/// the Interpolator.
+///
+/// We cannot call the above RegisterElement directly from execute_next_phase
+/// because that would result in a single call to RegisterElement rather
+/// than one call per Element.
+///
+/// Uses: nothing
+///
+/// DataBox changes:
+/// - Adds: nothing
+/// - Removes: nothing
+/// - Modifies: nothing
+///
+struct RegisterElementWithInterpolator {
+  template<
+    typename DbTags, typename... InboxTags, typename Metavariables,
+    typename ArrayIndex, typename ActionList, typename ParallelComponent>
+      static void apply(const db::DataBox<DbTags>& /*box*/,
+                    const tuples::TaggedTuple<InboxTags...>& /*inboxes*/,
+                    const Parallel::ConstGlobalCache<Metavariables>& cache,
+                    const ArrayIndex& /*array_index*/,
+                    const ActionList /*meta*/,
+                    const ParallelComponent* const /*meta*/) noexcept {
+    auto& interpolator =
+        *Parallel::get_parallel_component<::intrp::Interpolator<Metavariables>>(
+             cache)
+             .ckLocalBranch();
+    Parallel::simple_action<RegisterElement>(interpolator);
   }
 };
 

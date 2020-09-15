bc98808d82473f3582ed0b81688a65da5f054e18
Mark Scheel <scheel@tapir.caltech.edu>
2020-09-15 12:43:43 -0700

    Fixup: Fix documentation and add new figure.

--- src/Domain/CoordinateMaps/FocallyLiftedMap.hpp
+++ src/Domain/CoordinateMaps/FocallyLiftedMap.hpp
@@ -32,20 +32,33 @@ bool operator==(const FocallyLiftedMap<InnerMap>& lhs,
  * \brief Map from \f$(\bar{x},\bar{y},\bar{z})\f$ to the volume
  * contained between a 2D surface and the surface of a 2-sphere.
  *
+ *
+ * \image html FocallyLifted.svg "2D representation of focally lifted map."
+ *
  * \details We are given the radius \f$R\f$ and
- * center \f$C\f$ of a sphere, and a projection point \f$P\f$.  Also
+ * center \f$C^i\f$ of a sphere, and a projection point \f$P^i\f$.  Also,
+ * through the class defined by the `InnerMap` template parameter,
  * we are given the functions
  * \f$f^i(\bar{x},\bar{y},\bar{z})\f$ and
  * \f$\sigma(\bar{x},\bar{y},\bar{z})\f$ defined below; these
  * functions define the mapping from \f$(\bar{x},\bar{y},\bar{z})\f$
  * to the 2D surface.
  *
+ * The above figure is a 2D representation of the focally lifted map,
+ * where the shaded region in \f$\bar{x}^i\f$ coordinates on the left
+ * is mapped to the shaded region in the \f$x^i\f$ coordinates on the
+ * right.  Shown is an arbitrary point \f$\bar{x}^i\f$ that is mapped
+ * to \f$x^i\f$; for that point, the corresponding \f$x_0^i\f$ and
+ * \f$x_1^i\f$ (defined below) are shown on the right, as is the
+ * projection point \f$P^i\f$.  Also shown are the level surfaces
+ * \f$\sigma=0\f$ and \f$\sigma=1\f$.
+ *
  * The input coordinates are labeled \f$(\bar{x},\bar{y},\bar{z})\f$.
  * Let \f$x_0^i = f^i(\bar{x},\bar{y},\bar{z})\f$ be the three coordinates
  * of the 2D surface in 3D space.
  *
  * Now let \f$x_1^i\f$ be a point on the surface of the sphere,
- * constructed so that \f$P\f$, \f$x_0^i\f$, and \f$x_1^i\f$ are
+ * constructed so that \f$P^i\f$, \f$x_0^i\f$, and \f$x_1^i\f$ are
  * co-linear.  In particular, \f$x_1^i\f$ is determined by the
  * equation
  *
@@ -56,27 +69,46 @@ bool operator==(const FocallyLiftedMap<InnerMap>& lhs,
  * equation is derived by demanding that \f$x_1^i\f$ lies on the sphere:
  *
  * \f{align}
- * |P^i + (x_0^i - P^i) \lambda - C^i |^2 - R^2 = 0.
+ * |P^i + (x_0^i - P^i) \lambda - C^i |^2 - R^2 = 0,
  * \f}
  *
- * Now assume that \f$x_0^i\f$ corresponds to a level surface defined
- * by some scalar function \f$\sigma(\bar{x},\bar{y},\bar{z})=0\f$,
+ * where \f$|A^i|^2\f$ means \f$\delta_{ij} A^i A^j\f$.
+ *
+ * The quadratic equation, Eq. (2),
+ * takes the usual form \f$a\lambda^2+b\lambda+c=0\f$,
+ * with
+ *
+ * \f{align*}
+ *  a &= |x_0^i-P^i|^2,\\
+ *  b &= 2(x_0^i-P^i)(P^j-C^j)\delta_{ij},\\
+ *  c &= |P^i-C^i|^2 - R^2.
+ * \f}
+ *
+ * Now assume that \f$x_0^i\f$ lies on a level surface defined
+ * by some scalar function \f$\sigma(\bar{x}^i)=0\f$,
  * where \f$\sigma\f$ is normalized so that \f$\sigma=1\f$ on the sphere.
  * Then, once \f$\lambda\f$ has been computed and \f$x_1^i\f$ has
  * been determined, the map is given by
  *
- * \f{align}x^i = x_0^i + (x_1^i - x_0^i) \sigma(\bar{x},\bar{y},\bar{z}).\f}
+ * \f{align}x^i = x_0^i + (x_1^i - x_0^i) \sigma(\bar{x}^i).\f}
+ *
+ * Note that classes using FocallyLiftedMap will place restrictions on
+ * \f$P^i\f$, \f$C^i\f$, \f$x_0^i\f$, and \f$R\f$.  For example, we
+ * demand that \f$P^i\f$ does not lie on either the \f$\sigma=0\f$ or
+ * \f$\sigma=1\f$ surfaces depicted in the right panel of the figure,
+ * and we demand that the \f$\sigma=0\f$ and \f$\sigma=1\f$ surfaces do
+ * not intersect; otherwise the map is singular.
  *
  * ### Jacobian
  *
  * Differentiating Eq. (1) above yields
  *
- * \f[
- * \frac{\partial x_1^i}{\partial x_0^j} = \lambda +
+ * \f{align}
+ * \frac{\partial x_1^i}{\partial x_0^j} = \lambda \delta^i_j +
  *  (x_0^i - P^i) \frac{\partial \lambda}{\partial x_0^j}.
- * \f]
+ * \f}
  *
- * and differentiating Eq. (2) above yields
+ * and differentiating Eq. (2) and then inserting Eq. (1) yields
  *
  * \f{align}
  * \frac{\partial\lambda}{\partial x_0^j} &=
@@ -84,42 +116,49 @@ bool operator==(const FocallyLiftedMap<InnerMap>& lhs,
  * + (x_1^i - P^i)(P_i - C_{i})}.
  * \f}
  *
- * The Jacobian can be found by differentiating Eq. (3) above and combining
- * with the last two equations:
+ * The Jacobian can be found by differentiating Eq. (3) above and using the
+ * chain rule, recognizing that \f$x_1^i\f$ depends on \f$\bar{x}^i\f$ only
+ * through its dependence on \f$x_0^i\f$; this is because there is no explicit
+ * dependence on \f$\bar{x}^i\f$ in Eq. (1) (which determines \f$x_1^i\f$)
+ * or Eq. (2) (which determines \f$\lambda\f$).
  *
- * \f[
+ * \f{align}
  * \frac{\partial x^i}{\partial \bar{x}^j} =
  * \sigma \frac{\partial x_1^i}{\partial x_0^k}
  * \frac{\partial x_0^k}{\partial \bar{x}^j} +
  * (1-\sigma)\frac{\partial x_0^i}{\partial \bar{x}^j}
  * + \frac{\partial \sigma}{\partial \bar{x}^j} (x_1^i - x_0^i).
- * \f]
+ * \f}
  *
- * The inner map provides the function `deriv_sigma`, which returns
+ * The class defined by the `InnerMap` template parameter
+ * provides the function `deriv_sigma`, which returns
  * \f$\partial \sigma/\partial \bar{x}^j\f$, and the function `jacobian`,
  * which returns \f$\partial x_0^k/\partial \bar{x}^j\f$.
  *
  * ### Inverse map.
  *
- * Given \f$x^i\f$, we wish to compute \f$\bar{x}\f$,
- * \f$\bar{y}\f$, and \f$\bar{z}\f$.
+ * Given \f$x^i\f$, we wish to compute \f$\bar{x}^i\f$.
  *
  * We first find the coordinates \f$x_0^i\f$ that lie on the 2-surface
- * and are defined such that \f$P\f$, \f$x_0^i\f$, and \f$x^i\f$ are co-linear.
+ * and are defined such that \f$P^i\f$, \f$x_0^i\f$,
+ * and \f$x^i\f$ are co-linear.
+ * See the right panel of the above figure.
  * \f$x_0^i\f$ is determined by the equation
  *
  * \f{align} x_0^i = P^i + (x^i - P^i) \tilde{\lambda},\f}
  *
  * where \f$\tilde{\lambda}\f$ is a scalar factor that depends on
- * \f$x^i\f$ and is determined by the inner map.  The inner map
+ * \f$x^i\f$ and is determined by the class defined by the `InnerMap` template
+ * parameter.  `InnerMap`
  * provides a function `lambda_tilde` that takes \f$x^i\f$ and
  * \f$P^i\f$ as arguments and returns \f$\tilde{\lambda}\f$ (or
  * boost::none if the appropriate \f$\tilde{\lambda}\f$ cannot be
  * found; a value of boost::none indicates that the point \f$x^i\f$ is
  * outside the range of the map).
  *
- * Now consider the coordinates \f$x_1^i\f$ that lie on the sphere
- * and are defined such that \f$P\f$, \f$x_1^i\f$, and \f$x^i\f$ are co-linear.
+ * Now consider the coordinates \f$x_1^i\f$ that lie on the sphere and
+ * are defined such that \f$P^i\f$, \f$x_1^i\f$, and \f$x^i\f$ are
+ * co-linear.  See the right panel of the figure.
  * \f$x_1^i\f$ is determined by the equation
  *
  * \f{align} x_1^i = P^i + (x^i - P^i) \bar{\lambda},\f}
@@ -132,17 +171,34 @@ bool operator==(const FocallyLiftedMap<InnerMap>& lhs,
  * |P^i + (x^i - P^i) \bar{\lambda} - C^i |^2 - R^2 = 0.
  * \f}
  *
+ * Eq. (9) is a quadratic equation that
+ * takes the usual form \f$a\bar{\lambda}^2+b\bar{\lambda}+c=0\f$,
+ * with
+ *
+ * \f{align*}
+ *  a &= |x^i-P^i|^2,\\
+ *  b &= 2(x^i-P^i)(P^j-C^j)\delta_{ij},\\
+ *  c &= |P^i-C^i|^2 - R^2.
+ * \f}
+ *
  * Note that we don't actually need to compute \f$x_1^i\f$. Instead, we
- * can determine \f$\sigma\f$ by the relation
+ * can determine \f$\sigma\f$ by the relation (obtained by solving Eq. (3)
+ * for \f$\sigma\f$ and then inserting Eqs. (7) and (8) to eliminate
+ * \f$x_1^i\f$ and \f$x_0^i\f$)
  *
  * \f{align}
  * \sigma = \frac{\tilde{\lambda}-1}{\tilde{\lambda}-\bar{\lambda}}.
  * \f}
  *
+ * The denominator of Eq. (10) is nonzero for nonsingular maps:
+ * From Eqs. (7) and (8), \f$\bar{\lambda}=\tilde{\lambda}\f$ means
+ * that \f$x_1^i=x_0^i\f$, which means that
+ * the \f$\sigma=0\f$ and \f$\sigma=1\f$ surfaces intersect, i.e.
+ * the map is singular.
+ *
  * Once we have \f$x_0^i\f$ and \f$\sigma\f$, the point
- * \f$(\bar{x},\bar{y},\bar{z})\f$ is uniquely determined by the inner
- * map.
- * The `inverse` function of the inner map takes \f$x_0^i\f$ and \f$\sigma\f$
+ * \f$(\bar{x},\bar{y},\bar{z})\f$ is uniquely determined by `InnerMap`.
+ * The `inverse` function of `InnerMap` takes \f$x_0^i\f$ and \f$\sigma\f$
  * as arguments, and returns
  * \f$(\bar{x},\bar{y},\bar{z})\f$, or boost::none if \f$x_0^i\f$ or
  * \f$\sigma\f$ are outside the range of the map.
@@ -155,10 +211,10 @@ bool operator==(const FocallyLiftedMap<InnerMap>& lhs,
  * Suppose we are given \f$x^i\f$, and we have computed \f$\bar{x}^i\f$
  * by the above procedure.  We then correct \f$\bar{x}^i\f$ by adding
  *
- * \f[
+ * \f{align}
  * \delta \bar{x}^i = \left(x^j - x^j(\bar{x})\right)
  * \frac{\partial \bar{x}^i}{\partial x^j},
- * \f]
+ * \f}
  *
  * where \f$x^j(\bar{x})\f$ is the result of applying the forward map
  * to \f$\bar{x}^i\f$ and \f$\partial \bar{x}^i/\partial x^j\f$ is the
@@ -180,12 +236,12 @@ bool operator==(const FocallyLiftedMap<InnerMap>& lhs,
  * \f$x_0^k\f$ (the corresponding point on the 2-surface) and on
  * \f$\sigma\f$ (encoding the distance away from the 2-surface).
  *
- * We now evaluate Eq. (9). The inner map provides a function
+ * We now evaluate Eq. (12). The `InnerMap` class provides a function
  * `inv_jacobian` that returns \f$\partial \bar{x}^i/\partial x_0^k\f$
  * (where \f$\sigma\f$ is held fixed), and a function `dxbar_dsigma`
  * that returns \f$\partial \bar{x}^i/\partial \sigma\f$ (where
  * \f$x_0^i\f$ is held fixed).  The factor \f$\partial x_0^j/\partial
- * x^i\f$ can be computed by differentiating Eq. (5), which yields
+ * x^i\f$ can be computed by differentiating Eq. (7), which yields
  *
  * \f{align}
  * \frac{\partial x_0^j}{\partial x^i} &= \tilde{\lambda} \delta_i^j
@@ -193,13 +249,18 @@ bool operator==(const FocallyLiftedMap<InnerMap>& lhs,
  *   \frac{\partial\tilde{\lambda}}{\partial x^i},
  * \f}
  *
- * where \f$\partial \tilde{\lambda}/\partial x^i\f$ is provided
- * by the `deriv_lambda_tilde` function of the inner map.
+ * where \f$\partial \tilde{\lambda}/\partial x^i\f$ is provided by
+ * the `deriv_lambda_tilde` function of `InnerMap`.  Note that for
+ * nonsingular maps there is no worry that \f$\tilde{\lambda}\f$ is
+ * zero in the denominator of the second term of Eq. (13); if
+ * \f$\tilde{\lambda}=0\f$ then $x_0^i=P^i$ by Eq. (7), and therefore
+ * the map is singular.
  *
- * To evaluate the remaining unknown factor in Eq. (9),
+ * To evaluate the remaining unknown factor in Eq. (12),
  * \f$\partial \sigma/\partial x^j\f$,
- * note that \f$\bar{\lambda}=\tilde{\lambda}\lambda\f$.
- * Therefore Eq. (8) is equivalent to
+ * note that [combining Eqs. (1), (7), and (8)]
+ * \f$\bar{\lambda}=\tilde{\lambda}\lambda\f$.
+ * Therefore Eq. (10) is equivalent to
  *
  * \f{align}
  * \sigma &= \frac{\tilde{\lambda}-1}{\tilde{\lambda}(1-\lambda)}.
@@ -223,7 +284,10 @@ bool operator==(const FocallyLiftedMap<InnerMap>& lhs,
  * \frac{\partial \tilde\lambda}{\partial x^i},
  * \f}
  *
- * where the second factor in the first term can be evaluated using Eq. (4).
+ * where the second factor in the first term can be evaluated using
+ * Eq. (5), the third factor in the first term can be evaluated using
+ * Eq. (13), and the second factor in the second term is provided by
+ * `InnerMap`s function `deriv_lambda_tilde`.
  *
  */
 template <typename InnerMap>

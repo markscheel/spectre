4826a79fea81f8ded0eabcb9b8c2856ffd148c75
Mark Scheel <scheel@tapir.caltech.edu>
2021-08-05 15:36:18 -0700

    Add Test_StrahlkorperInDifferentFrame

---
+++ tests/Unit/ApparentHorizons/Test_StrahlkorperInDifferentFrame.cpp
@@ -0,0 +1,15 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#include "Framework/TestingFramework.hpp"
+
+#include <array>
+#include <random>
+
+#include "ApparentHorizons/StrahlkorperInDifferentFrame.hpp"
+#include "ApparentHorizons/SpherepackIterator.hpp"
+#include "ApparentHorizons/Strahlkorper.hpp"
+#include "DataStructures/Tensor/IndexType.hpp"
+#include "Framework/TestHelpers.hpp"
+#include "Utilities/Gsl.hpp"
+

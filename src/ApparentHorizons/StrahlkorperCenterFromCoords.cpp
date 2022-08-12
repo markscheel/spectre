cc0a210ff1aefac43a73b5404d726190e1d44b8d
Mark Scheel <scheel@tapir.caltech.edu>
2022-08-12 16:52:59 -0700

    fixup

--- src/ApparentHorizons/StrahlkorperCenterFromCoords.cpp
+++ src/ApparentHorizons/StrahlkorperCenterFromCoords.cpp
@@ -10,21 +10,24 @@
 #include "Utilities/Gsl.hpp"
 
 template <typename Frame>
-std::array<double, 3> strahlkorper_center_from_coords(
-    const tnsr::I<DataVector, 3, Frame>& cartesian_coords) {
+std::array<double, 3> approx_strahlkorper_center(
+    const tnsr::I<DataVector, 3, Frame>& cartesian_coords_on_surface) {
+  // Simply averages the coordinates on the surface.
+  // This is why it is approximate.
   std::array<double, 3> center{};
   for (size_t d = 0; d < 3; ++d) {
-    gsl::at(center, d) = std::accumulate(cartesian_coords.get(d).begin(),
-                                         cartesian_coords.get(d).end(), 0.0) /
-                         cartesian_coords.get(d).size();
+    gsl::at(center, d) =
+        std::accumulate(cartesian_coords_on_surface.get(d).begin(),
+                        cartesian_coords_on_surface.get(d).end(), 0.0) /
+        cartesian_coords_on_surface.get(d).size();
   }
   return center;
 }
 
 #define FRAME(data) BOOST_PP_TUPLE_ELEM(0, data)
-#define INSTANTIATE(_, data)                     \
-  template void strahlkorper_in_different_frame( \
-      const tnsr::I<DataVector, 3, Frame>& cartesian_coords);
+#define INSTANTIATE(_, data)                \
+  template void approx_strahlkorper_center( \
+      const tnsr::I<DataVector, 3, Frame>& cartesian_coords_on_surface);
 GENERATE_INSTANTIATIONS(INSTANTIATE,
                         (::Frame::Grid, ::Frame::Inertial, ::Frame::Distorted))
 
--- src/ApparentHorizons/StrahlkorperCenterFromCoords.hpp
+++ src/ApparentHorizons/StrahlkorperCenterFromCoords.hpp
@@ -11,9 +11,12 @@
 /// \brief Computes approximate expansion center of a Strahlkorper, given
 /// a set of points on the Strahlkorper surface.
 ///
+/// The algorithm is simple: it averages the points on the surface.
+/// This is why the center is approximate.
+///
 /// Used primarily when trying to evaluate a Strahlkorper in a
-/// different frame, in which case `cartesian_coords` is typically
+/// different frame, in which case `cartesian_coords_on_surface` is typically
 /// computed using `strahlkorper_coords_in_different_frame`.
 template <typename Frame>
-std::array<double, 3> strahlkorper_center_from_coords(
-    const tnsr::I<DataVector, 3, Frame>& cartesian_coords);
+std::array<double, 3> approx_strahlkorper_center(
+    const tnsr::I<DataVector, 3, Frame>& cartesian_coords_on_surface);

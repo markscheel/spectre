e27885adee7b75fa2775dcbe36167b5a08bcc60a
Mark Scheel <scheel@tapir.caltech.edu>
2025-08-28 12:31:14 -0700

    Add SparseMatrixCollection

---
+++ src/DataStructures/SparseMatrixCollection.hpp
@@ -0,0 +1,15 @@
+// Distributed under the MIT License.
+// See LICENSE.txt for details.
+
+#pragma once
+
+/*!
+ * \brief Holds a set of sparse matrices that transform between
+ * tensor components.
+ *
+ * 
+ *
+ */
+class SparseMatrixCollection {
+  std::vector<
+};

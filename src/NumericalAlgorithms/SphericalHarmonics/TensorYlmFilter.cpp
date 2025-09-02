5e1e8c2b7dd5da2330e0a913782e980d82c51695
Mark Scheel <scheel@tapir.caltech.edu>
2025-09-02 12:43:37 -0700

    FixZZ

--- src/NumericalAlgorithms/SphericalHarmonics/TensorYlmFilter.cpp
+++ src/NumericalAlgorithms/SphericalHarmonics/TensorYlmFilter.cpp
@@ -567,15 +567,15 @@ template FillFilter<typename tnsr::i::structure>(
     gsl::not_null<blaze::CompressedMatrix<double, blaze::rowMajor>*> matrix,
     size_t ell_max, size_t number_of_ell_modes_to_kill,
     std::optional<size_t> half_power);
-template FillFilter<typename tnsr::ii::structure>(
+template FillFilter<typename tnsr::ii<DataVector, 3>::structure>(
     gsl::not_null<blaze::CompressedMatrix<double, blaze::rowMajor>*> matrix,
     size_t ell_max, size_t number_of_ell_modes_to_kill,
     std::optional<size_t> half_power);
-template FillFilter<typename tnsr::ij::structure>(
+template FillFilter<typename tnsr::ij<DataVector, 3>::structure>(
     gsl::not_null<blaze::CompressedMatrix<double, blaze::rowMajor>*> matrix,
     size_t ell_max, size_t number_of_ell_modes_to_kill,
     std::optional<size_t> half_power);
-template FillFilter<typename tnsr::ijj::structure>(
+template FillFilter<typename tnsr::ijj<DataVector, 3>::structure>(
     gsl::not_null<blaze::CompressedMatrix<double, blaze::rowMajor>*> matrix,
     size_t ell_max, size_t number_of_ell_modes_to_kill,
     std::optional<size_t> half_power);

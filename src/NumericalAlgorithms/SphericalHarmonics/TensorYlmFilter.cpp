c4b052644f30cd057dc10742042538e38d99ba67
Mark Scheel <scheel@tapir.caltech.edu>
2025-09-23 16:54:08 -0700

    Move couts

--- src/NumericalAlgorithms/SphericalHarmonics/TensorYlmFilter.cpp
+++ src/NumericalAlgorithms/SphericalHarmonics/TensorYlmFilter.cpp
@@ -334,23 +334,25 @@ void inner_loops_three(
                       coef3j * threej_pq * threej_uv * threej_r * threej_w *
                       threej_mhat(l_dest) * threej_mcheck(l_dest) * sign_lhat *
                       sign_mtilde * symm_factor;
-                  if(correction.real() == 0.0 or correction.imag() == 0.0) {
-                    std::cout
-                        << "Corr =" << correction << " l_dest=" << l_dest
-                        << " m_dest=" << m_dest << " m_src=" << m_src
-                        << " lbar=" << lbar << " lhat=" << lhat
-                        << " mprime=" << mprime << " mhat=" << mhat
-                        << " threej_mhat(l_dest)=" << threej_mhat(l_dest)
-                        << " threej_mcheck(l_dest)=" << threej_mcheck(l_dest)
-                        << " threej_pq=" << threej_pq
-                        << " threej_uv=" << threej_uv
-                        << " threej_r=" << threej_r << " threej_w=" << threej_w
-                        << " coef3j" << coef3j << std::endl;
-                  }
                   if (m_src > 0) {
                     // Main term, first term in Eq. (18)
                     // ReRe
                     if (correction.imag() == 0) {
+                      if (correction.real() == 0.0) {
+                        std::cout
+                            << "Corr =" << correction << " l_dest=" << l_dest
+                            << " m_dest=" << m_dest << " m_src=" << m_src
+                            << " lbar=" << lbar << " lhat=" << lhat
+                            << " mprime=" << mprime << " mhat=" << mhat
+                            << " threej_mhat(l_dest)=" << threej_mhat(l_dest)
+                            << " threej_mcheck(l_dest)="
+                            << threej_mcheck(l_dest)
+                            << " threej_pq=" << threej_pq
+                            << " threej_uv=" << threej_uv
+                            << " threej_r=" << threej_r
+                            << " threej_w=" << threej_w << " coef3j" << coef3j
+                            << std::endl;
+                      }
                       iter_src.set(l_dest, static_cast<size_t>(m_src),
                                    SpherepackIterator::CoefficientArray::a);
                       iter_dest.set(l_dest, static_cast<size_t>(m_dest),
@@ -364,6 +366,21 @@ void inner_loops_three(
                                     SpherepackIterator::CoefficientArray::b);
                       add_element(correction.real());
                     } else {
+                      if (correction.imag() == 0.0) {
+                        std::cout
+                            << "Corr =" << correction << " l_dest=" << l_dest
+                            << " m_dest=" << m_dest << " m_src=" << m_src
+                            << " lbar=" << lbar << " lhat=" << lhat
+                            << " mprime=" << mprime << " mhat=" << mhat
+                            << " threej_mhat(l_dest)=" << threej_mhat(l_dest)
+                            << " threej_mcheck(l_dest)="
+                            << threej_mcheck(l_dest)
+                            << " threej_pq=" << threej_pq
+                            << " threej_uv=" << threej_uv
+                            << " threej_r=" << threej_r
+                            << " threej_w=" << threej_w << " coef3j" << coef3j
+                            << std::endl;
+                      }
                       // ReIm
                       iter_src.set(l_dest, static_cast<size_t>(m_src),
                                    SpherepackIterator::CoefficientArray::b);
@@ -382,6 +399,21 @@ void inner_loops_three(
                     // Second term in Eq. (18)
                     const double sign = (m_src % 2 == 0 ? 1.0 : -1.0);
                     if (correction.imag() == 0) {
+                      if (correction.real() == 0.0) {
+                        std::cout
+                            << "Corr =" << correction << " l_dest=" << l_dest
+                            << " m_dest=" << m_dest << " m_src=" << m_src
+                            << " lbar=" << lbar << " lhat=" << lhat
+                            << " mprime=" << mprime << " mhat=" << mhat
+                            << " threej_mhat(l_dest)=" << threej_mhat(l_dest)
+                            << " threej_mcheck(l_dest)="
+                            << threej_mcheck(l_dest)
+                            << " threej_pq=" << threej_pq
+                            << " threej_uv=" << threej_uv
+                            << " threej_r=" << threej_r
+                            << " threej_w=" << threej_w << " coef3j" << coef3j
+                            << std::endl;
+                      }
                       // ReRe
                       iter_src.set(l_dest, static_cast<size_t>(-m_src),
                                    SpherepackIterator::CoefficientArray::a);
@@ -396,6 +428,21 @@ void inner_loops_three(
                                     SpherepackIterator::CoefficientArray::b);
                       add_element(-sign * correction.real());
                     } else {
+                      if (correction.imag() == 0.0) {
+                        std::cout
+                            << "Corr =" << correction << " l_dest=" << l_dest
+                            << " m_dest=" << m_dest << " m_src=" << m_src
+                            << " lbar=" << lbar << " lhat=" << lhat
+                            << " mprime=" << mprime << " mhat=" << mhat
+                            << " threej_mhat(l_dest)=" << threej_mhat(l_dest)
+                            << " threej_mcheck(l_dest)="
+                            << threej_mcheck(l_dest)
+                            << " threej_pq=" << threej_pq
+                            << " threej_uv=" << threej_uv
+                            << " threej_r=" << threej_r
+                            << " threej_w=" << threej_w << " coef3j" << coef3j
+                            << std::endl;
+                      }
                       // ReIm
                       iter_src.set(l_dest, static_cast<size_t>(-m_src),
                                    SpherepackIterator::CoefficientArray::b);

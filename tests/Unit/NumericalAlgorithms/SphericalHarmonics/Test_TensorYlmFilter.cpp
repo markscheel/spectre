64924b148a10eb6541e7f96a354010d045d96f7a
Mark Scheel <scheel@tapir.caltech.edu>
2025-09-24 05:47:05 -0700

    Make into timing

--- tests/Unit/NumericalAlgorithms/SphericalHarmonics/Test_TensorYlmFilter.cpp
+++ tests/Unit/NumericalAlgorithms/SphericalHarmonics/Test_TensorYlmFilter.cpp
@@ -6,6 +6,8 @@
 #include <filesystem>
 #include <fstream>
 #include <optional>
+#include <chrono>
+#include <iostream>
 
 #include "DataStructures/DataVector.hpp"
 #include "DataStructures/SimpleSparseMatrix.hpp"
@@ -1028,29 +1030,40 @@ void test_tensorylm_filter_vs_spec(const size_t ell_max,
   }
 
   SparseMatrixType matrix;
+  const auto t0 = std::chrono::steady_clock::now();
   ylm::TensorYlm::FillFilter<TensorStructure>(
       make_not_null(&matrix), ell_max, number_of_ell_modes_to_kill, half_power);
+  const auto t1 = std::chrono::steady_clock::now();
+  std::cout << symm_to_string<TensorStructure>() << ", ell_max = " << ell_max
+            << ", time(ms)= "
+            << static_cast<size_t>(
+                   std::chrono::duration_cast<std::chrono::milliseconds>(t1 -
+                                                                         t0)
+                   .count())
+            << std::endl;
 
-  // Loop over spec_matrix_elements and make sure all the cases agree.
-  for (size_t i = 0; i < spec_matrix_elements.size(); ++i) {
-    CAPTURE(spec_dest_indices[i]);
-    CAPTURE(spec_src_indices[i]);
-    CHECK(matrix(spec_dest_indices[i], spec_src_indices[i]) ==
-          approx(spec_matrix_elements[i]));
-  }
+  // // Loop over spec_matrix_elements and make sure all the cases agree.
+  // for (size_t i = 0; i < spec_matrix_elements.size(); ++i) {
+  //   CAPTURE(spec_dest_indices[i]);
+  //   CAPTURE(spec_src_indices[i]);
+  //   CHECK(matrix(spec_dest_indices[i], spec_src_indices[i]) ==
+  //         approx(spec_matrix_elements[i]));
+  // }
 }
 }  // namespace
 
 // For debug builds, it is slow even for testing ell_max = 8 for all ranks,
 // so we need to increase the timeout.
 // Release builds are still under 10 seconds (barely).
-// [[TimeOut, 120]]
+// [[TimeOut, 1200]]
 SPECTRE_TEST_CASE("Unit.SphericalHarmonics.TensorYlmFilter",
                   "[NumericalAlgorithms][Unit]") {
-  const size_t ell_max = 8;
+  // const size_t ell_max = 8;
   const size_t num_to_kill = 4;
+  const auto half_power = std::optional<size_t>(28);
 
-  for (auto half_power : {std::optional<size_t>(), std::optional<size_t>(28)}) {
+  // for (auto half_power : {std::optional<size_t>(), std::optional<size_t>(28)}) {
+  for (size_t ell_max = 9; ell_max <= 79; ell_max += 10) {
     test_tensorylm_filter_vs_spec<typename tnsr::i<DataVector, 3>::structure,
                                   SimpleSparseMatrix>(ell_max, num_to_kill,
                                                       half_power);

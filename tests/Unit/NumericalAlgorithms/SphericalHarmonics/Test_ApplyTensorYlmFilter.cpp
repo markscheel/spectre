6631a59b419cb586deebe73c7e094bd82ad56a4f
Mark Scheel <scheel@tapir.caltech.edu>
2026-02-13 06:06:04 -0800

    Fix

--- tests/Unit/NumericalAlgorithms/SphericalHarmonics/Test_ApplyTensorYlmFilter.cpp
+++ tests/Unit/NumericalAlgorithms/SphericalHarmonics/Test_ApplyTensorYlmFilter.cpp
@@ -306,7 +306,7 @@ void test_apply_filter(const size_t num_to_kill) {
                 CAPTURE(num_to_kill);
                 CAPTURE(tensor_b.rank());
                 CAPTURE(storage_index);
-                CAPTURE(Tag::type::name());
+                CAPTURE(pretty_type::short_name<Tag::type>());
                 CHECK(0.0 == approx(b[it() + offset]));
               }
             }

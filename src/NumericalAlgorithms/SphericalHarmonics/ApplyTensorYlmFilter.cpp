5c221a8f4f32f57bc8f0fea27cb74e258f3cbfa9
Mark Scheel <scheel@tapir.caltech.edu>
2026-02-12 18:05:32 -0800

    Fix

--- src/NumericalAlgorithms/SphericalHarmonics/ApplyTensorYlmFilter.cpp
+++ src/NumericalAlgorithms/SphericalHarmonics/ApplyTensorYlmFilter.cpp
@@ -368,8 +368,8 @@ void apply_tensor_ylm_filter(
       });
 
   tmpl::for_each<gh_spatial_vars_list<Frame::Grid>>(
-      [&gh_spatial_spectral_vars, &ell_max, &num_to_kill,
-       &it]<class Tag>(const tmpl::type_<Tag> /*meta*/) {
+      [&gh_spatial_spectral_vars, &ell_max,
+       &radial_extents]<class Tag>(const tmpl::type_<Tag> /*meta*/) {
         ylm::SpherepackIterator it(ell_max, ell_max, radial_extents, true);
         constexpr size_t num_independent_components =
             Tag::type::structure::size();

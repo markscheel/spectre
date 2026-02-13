f0cf9ca4a0b329db30c588cb73f7b8f566bb427d
Mark Scheel <scheel@tapir.caltech.edu>
2026-02-12 17:36:50 -0800

    Apply test - radial-pts=1

--- tests/Unit/NumericalAlgorithms/SphericalHarmonics/Test_ApplyTensorYlmFilter.cpp
+++ tests/Unit/NumericalAlgorithms/SphericalHarmonics/Test_ApplyTensorYlmFilter.cpp
@@ -164,8 +164,8 @@ void test_modal_nodal_invertibility() {
 }
 
 void test_apply_filter(const size_t num_to_kill) {
-  constexpr size_t radial_extents = 2;
-  constexpr size_t ell_max = 7;
+  constexpr size_t radial_extents = 1;
+  constexpr size_t ell_max = 14;
 
   const auto& ylm = ::ylm::get_spherepack_cache(ell_max);
   const size_t spectral_mesh_size = ylm.spectral_size() * radial_extents;
@@ -317,8 +317,8 @@ SPECTRE_TEST_CASE("Unit.SphericalHarmonics.ApplyTensorYlmFilter",
   test_break_spacetime_vars_into_spatial_pieces();
   test_transform_spatial_tensors_to_different_frame();
   test_modal_nodal_invertibility();
-  test_apply_filter(0);
-  test_apply_filter(3);
+  //  test_apply_filter(0);
+  test_apply_filter(5);
 }
 }  // namespace
 }  // namespace ylm::TensorYlm
